99f860a3d47b86a59b454521992ac61485a501f0
tjzjwzd99 <emiliai@163.com>
2020-04-20 02:22:01 +0800

    Site updated: 2020-04-20 02:22:01

--- js/snow3.js
+++ js/snow3.js
@@ -130,6 +130,3 @@ var snow = new snowFall({
     maxFlake: 60
 });
 snow.start();
-————————————————
-版权声明：本文为CSDN博主「秦时明月之君临天下」的原创文章，遵循CC 4.0 BY-SA版权协议，转载请附上原文出处链接及本声明。
-原文链接：https://blog.csdn.net/weixin_41287260/article/details/103050877
